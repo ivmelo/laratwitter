9e7663508ddc97e0d67c52f54d07c23cee612448
vagrant <meloivanilson@gmail.com>
2016-10-05 23:02:46 +0000

    Autenticação

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,7 @@ class RegisterController extends Controller
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
+            'username' => 'required|min:5|unique:users'
         ]);
     }
 
@@ -66,6 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'username' => $data['username']
         ]);
     }
 }
